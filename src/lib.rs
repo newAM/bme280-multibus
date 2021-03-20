f5940b108ed8a0233d71b85770f1f4e3b334f156
Alex M <alexmgit@protonmail.com>
2021-03-20 14:31:20 -0700

    Bump ftd2xx-embedded-hal dependency to 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! multiple bus options (3-wire SPI, 4-wire SPI, I2C).
 //!
 //! This is just another BME280 driver implementation that I made for my own
-//! usecases.
+//! usecases (I2C + continuous sampling).
 //!
 //! # Example
 //!
@@ -17,23 +17,20 @@
 //! #   hal::i2c::Transaction::write_read(0x76, vec![0xE1], vec![0; 7]),
 //! #   hal::i2c::Transaction::write_read(0x76, vec![0xF7], vec![0; 8]),
 //! # ]);
-//! use bme280::{
-//!     Address, Bme280, Config, CtrlMeas, Filter, Init, Mode, Oversampling, Sample, Settings,
-//!     Standby, Uninit,
+//! use bme280::{Bme280, Init, Sample, Standby, Uninit};
+//!
+//! const SETTINGS: bme280::Settings = bme280::Settings {
+//!     config: bme280::Config::reset()
+//!         .set_standby_time(bme280::Standby::Millis1000)
+//!         .set_filter(bme280::Filter::X16),
+//!     ctrl_meas: bme280::CtrlMeas::reset()
+//!         .set_osrs_t(bme280::Oversampling::X8)
+//!         .set_osrs_p(bme280::Oversampling::X8)
+//!         .set_mode(bme280::Mode::Normal),
+//!     ctrl_hum: bme280::Oversampling::X8,
 //! };
 //!
-//! const SETTINGS: Settings = Settings {
-//!     config: Config::reset()
-//!         .set_standby_time(Standby::Millis1000)
-//!         .set_filter(Filter::X16),
-//!     ctrl_meas: CtrlMeas::reset()
-//!         .set_osrs_t(Oversampling::X8)
-//!         .set_osrs_p(Oversampling::X8)
-//!         .set_mode(Mode::Normal),
-//!     ctrl_hum: Oversampling::X8,
-//! };
-//!
-//! let mut bme: Bme280<_, Uninit> = Bme280::new(i2c, Address::SdoGnd);
+//! let mut bme: Bme280<_, Uninit> = Bme280::new(i2c, bme280::Address::SdoGnd);
 //! let mut bme: Bme280<_, Init> = bme.init(&SETTINGS)?;
 //! let sample: Sample = bme.sample()?;
 //! # Ok::<(), hal::MockError>(())
