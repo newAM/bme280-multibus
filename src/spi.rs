bb186a8bb6bca4739383b0c7b35c88acf7789189
Alex <7845120+newAM@users.noreply.github.com>
2022-01-01 11:36:15 -0800

    spi: add an infallible GPIO implementation

--- src/spi.rs
+++ src/spi.rs
@@ -122,3 +122,114 @@ where
         })
     }
 }
+
+/// BME280 SPI bus implementation with an infallible GPIO
+pub mod infallible_gpio {
+    /// BME280 bus.
+    #[derive(Debug)]
+    pub struct Bme280Bus<SPI, CS> {
+        bus: SPI,
+        cs: CS,
+    }
+
+    impl<SPI, CS, E> Bme280Bus<SPI, CS>
+    where
+        SPI: embedded_hal::blocking::spi::Transfer<u8, Error = E>
+            + embedded_hal::blocking::spi::Write<u8, Error = E>,
+        CS: embedded_hal::digital::v2::OutputPin<Error = core::convert::Infallible>,
+    {
+        /// Creates a new `Bme280Bus` from a SPI peripheral and a chip select
+        /// digital I/O pin.
+        ///
+        /// # Safety
+        ///
+        /// The chip select pin must be high before being passed to this function.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// # use embedded_hal_mock as hal;
+        /// # let spi = hal::spi::Mock::new(&[]);
+        /// # struct Pin {};
+        /// # impl embedded_hal::digital::v2::OutputPin for Pin {
+        /// #     type Error = core::convert::Infallible;
+        /// #     fn set_low(&mut self) -> Result<(), Self::Error> { Ok(()) }
+        /// #     fn set_high(&mut self) -> Result<(), Self::Error> { Ok(()) }
+        /// # }
+        /// # let mut pin = Pin {};
+        /// use bme280::spi::infallible_gpio::Bme280Bus;
+        /// use embedded_hal::digital::v2::OutputPin;
+        ///
+        /// pin.set_high().unwrap();
+        /// let mut bme: Bme280Bus<_, _> = Bme280Bus::new(spi, pin);
+        /// # Ok::<(), hal::MockError>(())
+        /// ```
+        #[inline]
+        pub fn new(bus: SPI, cs: CS) -> Self {
+            Bme280Bus { bus, cs }
+        }
+
+        /// Free the SPI bus and CS pin from the W5500.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// # use embedded_hal_mock as hal;
+        /// # let spi = hal::spi::Mock::new(&[]);
+        /// # struct Pin {};
+        /// # impl embedded_hal::digital::v2::OutputPin for Pin {
+        /// #     type Error = core::convert::Infallible;
+        /// #     fn set_low(&mut self) -> Result<(), Self::Error> { Ok(()) }
+        /// #     fn set_high(&mut self) -> Result<(), Self::Error> { Ok(()) }
+        /// # }
+        /// # let mut pin = Pin {};
+        /// use bme280::spi::infallible_gpio::Bme280Bus;
+        /// use embedded_hal::digital::v2::OutputPin;
+        ///
+        /// pin.set_high().unwrap();
+        /// let mut bme: Bme280Bus<_, _> = Bme280Bus::new(spi, pin);
+        /// let (spi, pin) = bme.free();
+        /// # Ok::<(), hal::MockError>(())
+        /// ```
+        #[inline]
+        pub fn free(self) -> (SPI, CS) {
+            (self.bus, self.cs)
+        }
+
+        #[inline]
+        fn with_chip_enable<T, F>(&mut self, mut f: F) -> Result<T, E>
+        where
+            F: FnMut(&mut SPI) -> Result<T, E>,
+        {
+            self.cs.set_low().unwrap();
+            let result: Result<T, E> = f(&mut self.bus);
+            self.cs.set_high().unwrap();
+            result
+        }
+    }
+
+    impl<SPI, CS, E> crate::Bme280Bus for Bme280Bus<SPI, CS>
+    where
+        SPI: embedded_hal::blocking::spi::Transfer<u8, Error = E>
+            + embedded_hal::blocking::spi::Write<u8, Error = E>,
+        CS: embedded_hal::digital::v2::OutputPin<Error = core::convert::Infallible>,
+    {
+        type Error = E;
+
+        fn read_regs(&mut self, reg: u8, buf: &mut [u8]) -> Result<(), Self::Error> {
+            self.with_chip_enable(|spi| {
+                spi.write(&[reg | (1 << 7)])?;
+                spi.transfer(buf)?;
+                Ok(())
+            })
+        }
+
+        fn write_reg(&mut self, reg: u8, data: u8) -> Result<(), Self::Error> {
+            let mut buf: [u8; 2] = [reg & !(1 << 7), data];
+            self.with_chip_enable(|spi| {
+                spi.transfer(&mut buf)?;
+                Ok(())
+            })
+        }
+    }
+}
